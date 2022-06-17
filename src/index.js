6a43118b3615a64f270cf58cf060a5703780b00e
e-n-eliseev <e.n.eliseev@mail.ru>
2022-06-17 09:51:24 +0300

    for IE

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import { Provider } from 'react-redux';
 import store from './store';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
